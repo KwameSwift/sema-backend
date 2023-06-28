2a1a59b7651dc576c61b2e46c12eb4ff5b8cb42d
KwameSwift <fuakyeakyempim.charles@gmail.com>
2023-06-28 15:47:34 +0000

    Added profile image

--- Users/users_helper.py
+++ Users/users_helper.py
@@ -10,10 +10,13 @@ from Utilities.models.documents_model import UserDocuments
 LOCAL_FILE_PATH = os.environ.get("LOCAL_FILE_PATH")
 
 def upload_profile_image(profile_image, user):
+    
     base_directory = f"{LOCAL_FILE_PATH}{user.first_name}_{user.last_name}"
     full_directory = f"{base_directory}/User_Documents/Profile_Image"
+    if os.path.exists(full_directory):
+        os.remove(full_directory)
     profile_path = local_file_upload(full_directory, profile_image)
-
+    UserDocuments.objects.filter(owner=user, document_type="Profile Image").delete()
     user.profile_image = profile_path
     user.save()
 
