53d99855629e76baa1248f9637a7c2615c450f0f
KwameSwift <fuakyeakyempim.charles@gmail.com>
2023-07-08 14:39:38 +0000

    Added feed data

--- Users/urls.py
+++ Users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from Polls.views.polls_view import GetAllApprovedPollsByUser, GetMyPolls, SearchMyPolls
+from Polls.views.polls_view import GetAllApprovedPollsByUser, GetMyPolls
 
 from .views.users_views import (DeleteProfileImage, GetAuthorStatistics,
                                 GetMySinglePoll, GetUserBlogPosts, ProfileView,
