7e5469a698d3111137986bd6c09e31f435679060
KwameSwift <fuakyeakyempim.charles@gmail.com>
2023-09-03 19:42:20 +0000

    Fixed issues

--- chat_channels/consumers.py
+++ chat_channels/consumers.py
@@ -8,29 +8,27 @@ from channels.db import database_sync_to_async
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        print("Socket Connected")
+        await self.accept()
         chat_room_id = self.scope["url_route"]["kwargs"]["chat_room_id"]
-        try:
-            chat_room = await self.get_chat_room(chat_room_id)
-            room_name = str(chat_room.room_name).lower().replace(" ", "_")
-            await self.accept()
-            await self.channel_layer.group_add(room_name, self.channel_name)
-        except ChatRoom.DoesNotExist:
-            raise non_existing_data_exception("Chat Room")
+        chat_room = await self.get_chat_room(chat_room_id)
+        room_name = self.get_room_name(chat_room)
+        await self.channel_layer.group_add(room_name, self.channel_name)
 
     async def disconnect(self, close_code):
-        print("Socket Disconnected")
         chat_room_id = self.scope["url_route"]["kwargs"]["chat_room_id"]
-        try:
-            chat_room = await self.get_chat_room(chat_room_id)
-            room_name = str(chat_room.room_name).lower().replace(" ", "_")
-            await self.channel_layer.group_discard(room_name, self.channel_name)
-        except ChatRoom.DoesNotExist:
-            raise non_existing_data_exception("Chat Room")
+        chat_room = await self.get_chat_room(chat_room_id)
+        room_name = self.get_room_name(chat_room)
+        await self.channel_layer.group_discard(room_name, self.channel_name)
 
     async def send_group_messages(self, event):
         await self.send(json.dumps({"data": event["data"]}))
 
     @database_sync_to_async
     def get_chat_room(self, chat_room_id):
-        return ChatRoom.objects.get(id=chat_room_id)
+        try:
+            return ChatRoom.objects.get(id=chat_room_id)
+        except ChatRoom.DoesNotExist:
+            raise non_existing_data_exception("Chat Room")
+
+    def get_room_name(self, chat_room):
+        return str(chat_room.room_name).lower().replace(" ", "_")
